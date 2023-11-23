b1d2ddec9b64d685cc256b024a3fc00074419809
BlakeP253 <blakepudists@gmail.com>
2023-11-22 17:34:00 -0800

    did most of login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function LoginPage() {
+const  LoginPage = () => {
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState('');
+    
+    const handleUserName = (event) => {
+        setUsername(event.target.value);
+    };
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+    //this should log the user in (we could make it a set user/pass for example purposes)
+    const handleLogin = (event) => {
+
+    }
+
     return (
-        <div></div>
-    )
-}
+        <div calssName="container">
+            <h2> Login </h2>
+            <form>
+                <div className="container">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        className="container"
+                        id="username"
+                        value={username}
+                        onChange={handleUserName}
+                    />
+                </div>
+                <div className="container">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="text"
+                        className="container"
+                        id="password"
+                        value={password}
+                        onChange={handlePassword}
+                    />
+                </div>
+                <button type="button" className="btn" onClick={handleLogin}>
+                    Log in
+                </button>
+            </form>
+        </div>
+    ); 
+};
+
+export default LoginPage
