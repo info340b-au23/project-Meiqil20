5728ebcc8b338de8b3914eef40938ed955e49128
“BonieXCW” <boniewang77701339@gmail.com>
2023-12-11 19:15:15 -0800

    Start implementing LoginPage's feature

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { StyledFirebaseAuth } from 'react-firebaseui';
 
 export function LoginPage() {
+//   const currentUser = props.currentUser;
+//   const loginFunction = props.loginCallback;
+  
+  const auth = getAuth(); //the authenticator
 
-    return (
-        <div>
-            <header id="login_box">
-                <h1>LOGIN</h1>
-            </header>
-            <main>
-            <div id="login">
-                <label for="email">Email</label><br />
-                <input type="email" id="email" name="email" required/><br/>
-                <label for="password">Password</label><br/>
-                <input type="password" id="password" name="password" required/><br/>
-                <button type="button" id="button">Login</button>
-            </div>
-            <div id="sign_up">
-                <p>----------------------OR----------------------</p>
-                <p>Need an account? <a href="/">SIGN UP</a></p>
-            </div>
-        </main>
-    
-        </div>
-    );
-};
+  const configObj = {
+    signInOptions: [
+      { 
+        provider: EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: true,
+      },
+      {
+        provider: GoogleAuthProvider.PROVIDER_ID
+      }
+    ],
+    signInFlow: 'popup',
+    callbacks: {
+      signInSuccessWithAuthResult: () => false //don't do anything special on signin
+    },
+    credentialHelper: 'none'
+  }
+
+  return (
+    <div className="card bg-light">
+      <div className="container card-body">
+        <StyledFirebaseAuth firebaseAuth={auth} uiConfig={configObj} />
+ 
+        {/* <p className="lead">Pick a user:</p>
+        <Dropdown>
+...
+		</Dropdown> */}
+      </div>
+    </div>
+  )
+}
+
